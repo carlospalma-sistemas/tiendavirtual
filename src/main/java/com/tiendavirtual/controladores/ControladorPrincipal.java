d1646841fc581f5ef1520b2bb62ec72288cf5e71
Carlos Andrés Palma Suárez <34041415+carlospalma-sistemas@users.noreply.github.com>
2022-08-29 06:43:24 -0500

    Creadas páginas inicial y lista categorias

---
+++ src/main/java/com/tiendavirtual/controladores/ControladorPrincipal.java
@@ -0,0 +1,18 @@
+package com.tiendavirtual.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ControladorPrincipal {
+    
+    @GetMapping("/")
+    public String cargarPaginaPrincipal() {
+        return "index";
+    }
+    
+    @GetMapping("/categorias")
+    public String cargarListaCategorias() {
+        return "listacategorias";
+    }
+}
