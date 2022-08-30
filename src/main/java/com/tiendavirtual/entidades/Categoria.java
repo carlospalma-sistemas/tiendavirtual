b58a4aa3da47a87fbfc67d3830c29911170cab6e
Carlos Andrés Palma Suárez <34041415+carlospalma-sistemas@users.noreply.github.com>
2022-08-30 09:46:10 -0500

    Incorporada persistencia

--- src/main/java/com/tiendavirtual/entidades/Categoria.java
+++ src/main/java/com/tiendavirtual/entidades/Categoria.java
@@ -1,6 +1,63 @@
 package com.tiendavirtual.entidades;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name="tcategorias")
 public class Categoria {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    
+    @Column(name = "nombre", length = 45, nullable = false)
     private String nombre;
+    
+    @Column(name = "estante")
+    private int estante;
+    
+    @Column(name = "hab", columnDefinition = "TINYINT(1)")
+    private boolean hab;
+
+    public Categoria() {
+    }
+
+    public Categoria(String nombre, int estante) {
+        this.nombre = nombre;
+        this.estante = estante;
+    }
+
+    public Categoria(int id, String nombre, int estante) {
+        this.id = id;
+        this.nombre = nombre;
+        this.estante = estante;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEstante() {
+        return estante;
+    }
+
+    public void setEstante(int estante) {
+        this.estante = estante;
+    }
+    
 }
