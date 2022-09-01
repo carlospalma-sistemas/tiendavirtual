b7ec5861aa64d0c12ca9c79681daaf357146f14e
Carlos Andrés Palma Suárez <34041415+carlospalma-sistemas@users.noreply.github.com>
2022-09-01 17:59:18 -0500

    Añadir tests

--- src/test/java/com/tiendavirtual/TiendavirtualApplicationTests.java
+++ src/test/java/com/tiendavirtual/TiendavirtualApplicationTests.java
@@ -2,6 +2,7 @@ package com.tiendavirtual;
 
 import com.tiendavirtual.entidades.Categoria;
 import com.tiendavirtual.servicios.CategoriaServicio;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -22,4 +23,17 @@ class TiendavirtualApplicationTests {
         Assertions.assertTrue(guardado.getId() > 0, "ERROR, No se guardó el nuevo objeto");
     }
 
+    @Test
+    @Disabled
+    void probarSiEncuentroDamaEnCategorias() {
+        List<Categoria> listado = servicio.consultarCategorias("caballero");
+        Assertions.assertTrue(listado.size() > 0, "ERROR, No se encontró catgoría con el nombre dama");
+    }
+    
+    @Test
+    @Disabled
+    void probarSiEncuentroCategoriaConId1() {
+        Categoria encontrado = servicio.consultarCategorias(1);
+        Assertions.assertTrue(encontrado.getId() == 1, "ERROR, no se encontró categoría con id 1");
+    }
 }
