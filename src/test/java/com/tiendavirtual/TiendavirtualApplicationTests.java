2122e7446f4642f4ddbe3b6788ce18cd089a5c5c
Carlos Andrés Palma Suárez <34041415+carlospalma-sistemas@users.noreply.github.com>
2022-09-05 13:40:52 -0500

    Actualizar tests

--- src/test/java/com/tiendavirtual/TiendavirtualApplicationTests.java
+++ src/test/java/com/tiendavirtual/TiendavirtualApplicationTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 
 @SpringBootTest
 class TiendavirtualApplicationTests {
@@ -16,20 +17,18 @@ class TiendavirtualApplicationTests {
     private CategoriaServicio servicio;
     
     @Test
-    void probarSiSeCreaUnaNuevaCategoria() {
+    void probarSiNoSeCreaUnaCategoriaRepetida() {
         Categoria c = new Categoria("Camisas para dama", 1, true);
-        Assertions.assertDoesNotThrow(() -> servicio.crearNuevaCategoria(c), "Error al guardar nueva categoría. ");
+        Assertions.assertThrows(DataIntegrityViolationException.class, () -> servicio.crearNuevaCategoria(c), "Se esperaba error de violación de integridad");
     }
 
     @Test
-    @Disabled
     void probarSiEncuentroDamaEnCategorias() {
         List<Categoria> listado = servicio.consultarCategorias("dama");
         Assertions.assertTrue(listado.size() > 0, "Error, no se encontró categoría con la palabra dama");
     }
     
     @Test
-    @Disabled
     void probarSiEncuentroCategoriaConId1() {
         Categoria encontrado = servicio.consultarCategorias(1);
         Assertions.assertNotNull(encontrado, "ERROR, no se encontró categoría con id 1");
